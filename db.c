4db31b82182f03140971cfd07af7c62145d242eb
jakobgokpinar <jakobgokpinar@gmail.com>
2026-03-29 13:58:57 +0200

    naming convention and includes

--- db.c
+++ db.c
@@ -1,3 +1,8 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 typedef struct {
     char* buffer;
     size_t buffer_length;
@@ -36,7 +41,12 @@ void read_input(InputBuffer* input_buffer) {
     input_buffer->buffer[bytes_read - 1] = 0;
 }
 
-int main(int argc, char* []argv) {
+void close_input_buffer(InputBuffer* input_buffer) {
+    free(input_buffer->buffer);
+    free(input_buffer);
+}
+
+int main(int argc, char* argv[]) {
     InputBuffer* input_buffer = new_input_buffer();
     while (true) {
         print_prompt();
