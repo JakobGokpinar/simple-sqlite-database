98e0cd2567feff20dfd9e2e6bbd108918d055be4
jakobgokpinar <jakobgokpinar@gmail.com>
2026-03-31 12:07:31 +0200

    added typedef enums

--- db.c
+++ db.c
@@ -3,11 +3,29 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef enum {
+    META_COMMAND_SUCCESS
+    META_COMMAND_UNRECEGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum {
+    PREPARE_SUCCESS
+    PREPARE_UNRECOGNIZED_STATMENT
+} PrepareResult;
+
 typedef struct {
     char* buffer;
     size_t buffer_length;
     ssize_t input_length;
 } InputBuffer;
+
+MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+    if (strcmp(input_buffer->buffer, ".exit") == 0) {
+        exit(EXIT_SUCCESS);
+    } else {
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
 
 InputBuffer* new_input_buffer() {
     InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
@@ -45,6 +63,7 @@ void close_input_buffer(InputBuffer* input_buffer) {
     free(input_buffer->buffer);
     free(input_buffer);
 }
+
 
 int main(int argc, char* argv[]) {
     InputBuffer* input_buffer = new_input_buffer();
