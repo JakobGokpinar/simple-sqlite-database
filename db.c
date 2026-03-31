08558ffe39dc3cb88fda96350fbed40ac918dff7
jakobgokpinar <jakobgokpinar@gmail.com>
2026-03-31 15:59:29 +0200

    Statement struct and prepare_statement

--- db.c
+++ db.c
@@ -4,14 +4,17 @@
 #include <string.h>
 
 typedef enum {
-    META_COMMAND_SUCCESS
-    META_COMMAND_UNRECEGNIZED_COMMAND
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
 } MetaCommandResult;
 
-typedef enum {
-    PREPARE_SUCCESS
-    PREPARE_UNRECOGNIZED_STATMENT
-} PrepareResult;
+typedef enum { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT } PrepareResult;
+
+typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType; 
+
+typedef struct {
+    StatementType type;
+} Statement;
 
 typedef struct {
     char* buffer;
@@ -19,12 +22,25 @@ typedef struct {
     ssize_t input_length;
 } InputBuffer;
 
+
 MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
     if (strcmp(input_buffer->buffer, ".exit") == 0) {
         exit(EXIT_SUCCESS);
     } else {
         return META_COMMAND_UNRECOGNIZED_COMMAND;
     }
+}
+
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    if (strcmp(input_buffer->buffer, "select") == 0) {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+    return PREPARE_UNRECOGNIZED_STATEMENT;
 }
 
 InputBuffer* new_input_buffer() {
@@ -62,6 +78,17 @@ void read_input(InputBuffer* input_buffer) {
 void close_input_buffer(InputBuffer* input_buffer) {
     free(input_buffer->buffer);
     free(input_buffer);
+}
+
+void execute_statement(Statement* statement) {
+    switch(statement->type) {
+        case(STATEMENT_INSERT):
+            printf("This is where we would do an insert.\n");
+            break;
+        case(STATEMENT_SELECT):
+            printf("This is where we would do select.\n");
+            break;
+    }
 }
 
 
