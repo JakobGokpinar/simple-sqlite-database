d1de50d37a57a64466346b3e1945629d2e44f3a6
jakobgokpinar <jakobgokpinar@gmail.com>
2026-04-10 22:50:38 +0200

    cursor functions implemented

--- db.c
+++ db.c
@@ -328,11 +328,19 @@ Table* db_open(const char* filename) {
 }
 
 Cursor* table_start(Table* table) {
-
+    Cursor* cursor = malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = 0;
+    cursor->end_of_table = (table->num_rows == 0);
+    return cursor;
 }
 
 Cursor* table_end(Table* table) {
-    
+    Cursor* cursor = malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = table->num_rows;
+    cursor->end_of_table = true;
+    return cursor;
 }
 
 
