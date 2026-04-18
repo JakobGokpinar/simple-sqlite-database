50ef8e502d1d5319b90deaa221ee913ee2a38e8c
jakobgokpinar <jakobgokpinar@gmail.com>
2026-04-18 14:20:17 +0200

    formatting

--- db.c
+++ db.c
@@ -256,6 +256,8 @@ void cursor_advance(Cursor* cursor) {
     }
 }
 
+
+
 void pager_flush(Pager* pager, uint32_t page_num) {
     if (pager->pages[page_num] == NULL) {
         printf("Tried to flush null page\n");
@@ -482,21 +484,6 @@ Cursor* leaf_node_find(Table* table, uint32_t page_num, uint32_t key) {
     return cursor;
 }
 
-/*
-* Return the position of the given key. 
-* If the key is not present, return the index of where it should be inserted
-*/
-Cursor* table_find(Table* table, uint32_t key) {
-    uint32_t root_page_num = table->root_page_num;
-    void* root_node = get_page(table->pager, root_page_num);
-    if (get_node_type(root_node) == NODE_LEAF) {
-        return leaf_node_find(table, root_page_num, key);
-    } else {
-        printf("Need to implement searching an internal node.\n");
-        exit(EXIT_FAILURE);
-    }
-}
-
 Cursor* table_start(Table* table) {
     Cursor* cursor = malloc(sizeof(Cursor));
     cursor->table = table;
@@ -533,6 +520,20 @@ Cursor* internal_node_find(Table* table, uint32_t page_num, uint32_t key) {
             return leaf_node_find(table, child_num, key);
         case NODE_INTERNAL:
             return internal_node_find(table, child_num, key);
+    }
+}
+
+/*
+* Return the position of the given key. 
+* If the key is not present, return the index of where it should be inserted
+*/
+Cursor* table_find(Table* table, uint32_t key) {
+    uint32_t root_page_num = table->root_page_num;
+    void* root_node = get_page(table->pager, root_page_num);
+    if (get_node_type(root_node) == NODE_LEAF) {
+        return leaf_node_find(table, root_page_num, key);
+    } else {
+        return internal_node_find(table, root_page_num, key);
     }
 }
 
